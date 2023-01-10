cd693a71a90de560797799e1a20d532938523205
Sreya Sarathy <110914427+sreyasarathy@users.noreply.github.com>
2023-01-10 12:18:45 +0530

    Main file for the game

---
+++ main.py
@@ -0,0 +1,11 @@
+import random
+
+  number = random.randomInteger(1,10)
+  for i in range(0,3)
+  user = int(input("Guess the number"))
+  if user == number: 
+  print("You got it! You guessed the number!!) 
+  print(f "The number is {number}")
+  break
+  if user != number: 
+  print(f"Your guess is incorrect :( The number is {number}") 
